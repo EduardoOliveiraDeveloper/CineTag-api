aecf1add45fd70ee777f742c8e7942f7e4b480d5
Eduardo Silva Oliveira <126707455+EduardoOliveiraDeveloper@users.noreply.github.com>
2023-08-16 15:47:52 -0300

    Add files via upload

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+function Rodape (){
+    return(
+        <footer className={styles.rodape}>
+          <h2>Desenvolvido em curso pela plataforma Alura .</h2>  
+        </footer>
+    )
+}
+
+export default Rodape;
